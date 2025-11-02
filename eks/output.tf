2c8f3aeee3d3e3d39bbed756f8f1ebc643dc3eca
Nishkarsh9 <115409219+Nishkarsh9@users.noreply.github.com>
2025-11-03 02:26:16 +0530

    Update output.tf

--- eks/output.tf
+++ eks/output.tf
@@ -6,11 +6,32 @@ output "node_security_group_arn" {
 output "node_security_group_id" {
   value       = module.eks.node_security_group_id
   description = "ID of the EKS node security group"
-
 }
 
 output "eks_managed_node_groups" {
   value       = module.eks.eks_managed_node_groups
   description = "Details of the EKS managed node groups"
+}
 
+###########################################################
+# Outputs
+###########################################################
+output "vpc_id" {
+  description = "VPC ID used by EKS cluster"
+  value       = data.terraform_remote_state.network.outputs.vpc_id
+}
+
+output "cluster_id" {
+  description = "EKS cluster ID"
+  value       = module.eks.cluster_id
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "EKS cluster CA certificate data"
+  value       = module.eks.cluster_certificate_authority_data
 }
